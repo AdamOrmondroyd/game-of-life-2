957f90bbb2e956e8497399120e7f9da645d0bae5
Ormorod <Adam.Ormondroyd@gmail.com>
2021-11-19 14:34:58 +0000

    changed test functions to start with test rather than end in test

--- test_goy.py
+++ test_goy.py
@@ -4,13 +4,13 @@ Test goy.py
 import numpy as np
 from goy import step, run, BEACON_1, BEACON_2, GLIDER
 
-def oscillator_test():
+def test_oscillator():
     """Test whether step does a beacon oscillator correctly"""
     middle = step(BEACON_1)
     end = step(middle)
     assert(np.array_equal(BEACON_2, middle) and np.array_equal(BEACON_1, end))
     
-def glider_test():
+def test_glider():
     """Test whether running a glider 24 steps on a 6x6 gets back to where it started"""
     end_cells = run(GLIDER, 24)
     assert(np.array_equal(GLIDER, end_cells))
