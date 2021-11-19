141bb111f5d79935305952a63bde1a419052dbb0
Ormorod <Adam.Ormondroyd@gmail.com>
2021-11-19 12:52:31 +0000

    Changed 'Tests' to 'Test' in test functions

--- test_goy.py
+++ test_goy.py
@@ -5,12 +5,12 @@ import numpy as np
 from goy import step, run, BEACON_1, BEACON_2, GLIDER
 
 def oscillator_test():
-    """Tests whether step does a beacon oscillator correctly"""
+    """Test whether step does a beacon oscillator correctly"""
     middle = step(BEACON_1)
     end = step(middle)
     assert(np.array_equal(BEACON_2, middle) and np.array_equal(BEACON_1, end))
     
 def glider_test():
-    """Tests whether running a glider 24 steps on a 6x6 gets back to where it started"""
+    """Test whether running a glider 24 steps on a 6x6 gets back to where it started"""
     end_cells = run(GLIDER, 24)
     assert(np.array_equal(GLIDER, end_cells))
